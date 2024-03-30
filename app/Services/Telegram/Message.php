f09c63ad12be7f3fd6215997d63aab3f4d1adc9b
mortezapro <goodarzi.morteza73@gmail.com>
2024-03-30 12:49:15 +0330

    telegram

--- app/Services/Telegram/Message.php
+++ app/Services/Telegram/Message.php
@@ -34,7 +34,7 @@ class Message implements TelegramInterface {
             "chat_id"   => $this->update->getChat()->get("id"),
             "text"      => $this->update->message->text,
         ];
-        Log::info((object)$messageData);
+        Log::info(json_encode($messageData));
         if($this->userService->count( ["id" => $this->update->message->from->id]) == 0){
             $this->userService->store($userData);
         }
