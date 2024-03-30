e5687ee16705b34f8ff5b2e990739e37a84e21c9
mortezapro <goodarzi.morteza73@gmail.com>
2024-03-30 11:52:05 +0330

    telegram

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,6 +17,7 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
+        'id',
         'first_name',
         'last_name',
         'username',
