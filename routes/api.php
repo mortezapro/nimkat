fd9cd25b7c9b65b4ed8fbafcddf529b20be51853
mortezapro <goodarzi.morteza73@gmail.com>
2024-03-31 17:31:29 +0330

    telegram

--- routes/api.php
+++ routes/api.php
@@ -9,38 +9,3 @@ use Telegram\Bot\Laravel\Facades\Telegram;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
-
-
-Route::post('/telegram/webhook', [TelegramController::class,"handleWebhook"]);
-
-Route::get('/setWebhook', function () {
-    $url = 'https://103.75.199.54/api/telegram/webhook';
-    $response = Telegram::setWebhook([
-        'url' => $url,
-        'allowed_updates' => ['message', "callback_query","chat_member","message_reaction","message_reaction_count","edited_message"],
-        'encoding' => 'UTF-8',
-    ]);
-
-    echo $url;
-    return $response == true ? 'Webhook URL set!' : 'Failed to set webhook URL!';
-});
-
-Route::get('/removeWebhook', function () {
-    $response = Telegram::removeWebhook();
-    return $response == true ? 'Webhook URL removed!' : 'Failed to remove webhook URL!';
-});
-
-
-
-Route::get('/update', function () {
-    $response = Telegram::getUpdates();
-    dd($response);
-});
-
-
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
-
--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,26 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::post('v1/telegram/webhook', [TelegramController::class,"handleWebhook"]);
+
+Route::get('/setWebhook', function () {
+    $url = 'https://103.75.199.54/v1/telegram/webhook';
+    $response = Telegram::setWebhook([
+        'url' => $url,
+        'allowed_updates' => ['message', "callback_query","chat_member","message_reaction","message_reaction_count","edited_message"],
+        'encoding' => 'UTF-8',
+    ]);
+
+    echo $url;
+    return $response == true ? 'Webhook URL set!' : 'Failed to set webhook URL!';
+});
+
+Route::get('/removeWebhook', function () {
+    $response = Telegram::removeWebhook();
+    return $response == true ? 'Webhook URL removed!' : 'Failed to remove webhook URL!';
+});
+
+Route::get('/update', function () {
+    $response = Telegram::getUpdates();
+    dd($response);
+});
