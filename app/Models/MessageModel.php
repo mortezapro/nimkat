baf4a3e917292d861c20b8eba3d6055ce8624add
mortezapro <goodarzi.morteza73@gmail.com>
2024-04-01 01:16:40 +0330

    telegram

--- app/Models/MessageModel.php
+++ app/Models/MessageModel.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Morilog\Jalali\Jalalian;
 
 class MessageModel extends Model
 {
@@ -13,10 +14,25 @@ class MessageModel extends Model
     protected $fillable = [
         "id","chat_id","user_id","text"
     ];
+    protected $appends=["fullName","chat","date"];
 
     public function user()
     {
         return $this->belongsTo(User::class,"user_id");
     }
 
+    public function getFullNameAttribute()
+    {
+        return $this->user->first_name . "" .$this->user->last_name;
+    }
+    public function getDateAttribute()
+    {
+        return Jalalian::forge($this->created_at)->format("%d - %m - %d H:i");
+    }
+    public function getChatAttribute()
+    {
+        if($this->chat_id == "-1002068548070"){
+            return "نیمکت";
+        }
+    }
 }
