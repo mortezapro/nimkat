17ed1eac1bec5371637d5027523079e57b57a7d6
mortezapro <goodarzi.morteza73@gmail.com>
2024-04-01 19:52:59 +0330

    telegram

--- app/Services/Telegram/MessageReaction.php
+++ app/Services/Telegram/MessageReaction.php
@@ -29,14 +29,11 @@ class MessageReaction implements TelegramInterface{
         $newReaction = $this->update->message_reaction->new_reaction;
         Log::info("creating emoji");
         if (isset($this->update->message_reaction)) {
+            $emoji="default";
             $reaction = $this->update->message_reaction;
             if (isset($reaction->new_reaction) && !empty($reaction->new_reaction)) {
                 if (count($newReaction) > 0 && is_array($newReaction[0])) {
                     $emoji = $newReaction[0]["emoji"];
-                } elseif (is_object($newReaction[0])) {
-                    $emoji = $newReaction[0]->emoji;
-                } else {
-                    $emoji = null;
                 }
                 $data = [
                     "user_id" => $userId,
