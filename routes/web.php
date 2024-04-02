cff59c2745fa23afdcadd0e2d3f23fab99c54434
mortezapro <goodarzi.morteza73@gmail.com>
2024-04-02 13:55:26 +0330

    telegram

--- routes/web.php
+++ routes/web.php
@@ -19,42 +19,42 @@ Route::get('/', function () {
     ]);
 });
 
-//Route::get('/dashboard', function () {
-//    return Inertia::render('Dashboard');
-//})->middleware(['auth', 'verified'])->name('dashboard');
-//
-//Route::middleware('auth')->group(function () {
-//    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-//    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-//    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-//
-//    Route::resource("messages",MessageController::class);
-//    Route::resource("users",UserController::class);
-//});
-//
-//require __DIR__.'/auth.php';
-//
-//
-//Route::post('v2/telegram/webhook', [TelegramController::class,"handleWebhook"]);
-//
-//Route::get('/setWebhook', function () {
-//    $url = 'https://103.75.199.54/v2/telegram/webhook';
-//    $response = Telegram::setWebhook([
-//        'url' => $url,
-//        'allowed_updates' => ['message', "callback_query","chat_member","message_reaction","message_reaction_count","edited_message"],
-//        'encoding' => 'UTF-8',
-//    ]);
-//
-//    echo $url;
-//    return $response == true ? 'Webhook URL set!' : 'Failed to set webhook URL!';
-//});
-//
-//Route::get('/removeWebhook', function () {
-//    $response = Telegram::removeWebhook();
-//    return $response == true ? 'Webhook URL removed!' : 'Failed to remove webhook URL!';
-//});
-//
-//Route::get('/update', function () {
-//    $response = Telegram::getUpdates();
-//    dd($response);
-//});
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::resource("messages",MessageController::class);
+    Route::resource("users",UserController::class);
+});
+
+require __DIR__.'/auth.php';
+
+
+Route::post('v3/telegram/webhook', [TelegramController::class,"handleWebhook"]);
+
+Route::get('/setWebhook', function () {
+    $url = 'https://103.75.199.54/v3/telegram/webhook';
+    $response = Telegram::setWebhook([
+        'url' => $url,
+        'allowed_updates' => ['message', "callback_query","chat_member","message_reaction","message_reaction_count","edited_message"],
+        'encoding' => 'UTF-8',
+    ]);
+
+    echo $url;
+    return $response == true ? 'Webhook URL set!' : 'Failed to set webhook URL!';
+});
+
+Route::get('/removeWebhook', function () {
+    $response = Telegram::removeWebhook();
+    return $response == true ? 'Webhook URL removed!' : 'Failed to remove webhook URL!';
+});
+
+Route::get('/update', function () {
+    $response = Telegram::getUpdates();
+    dd($response);
+});
