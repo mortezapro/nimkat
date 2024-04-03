4b5843d920376539349741325da10d77e706dc16
mortezapro <goodarzi.morteza73@gmail.com>
2024-04-03 15:19:00 +0330

    old data

--- app/Http/Controllers/OldMessageController.php
+++ app/Http/Controllers/OldMessageController.php
@@ -19,7 +19,7 @@ class OldMessageController extends Controller
         while (!feof($fileHandle)) {
             $line = fgets($fileHandle);
             $message = json_decode($line, true);
-            echo $message;
+            echo is_array($message);
             if (isset($message['text'])) {
                 $words = explode(' ', $message['text']);
                 $wordsCollection = $wordsCollection->merge($words);
