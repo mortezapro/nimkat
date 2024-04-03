57ba813988a154ca37d3cf22e354fbd114a22c74
mortezapro <goodarzi.morteza73@gmail.com>
2024-04-03 13:57:57 +0330

    old data

--- app/Http/Controllers/OldMessageController.php
+++ app/Http/Controllers/OldMessageController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Redis;
 use Illuminate\Support\Facades\Storage;
 
@@ -11,19 +12,12 @@ class OldMessageController extends Controller
 {
     public function index()
     {
-//        $data = Redis::get("data");
-//        if(!$data){
-//            $data = Storage::disk('local')->get('data/data.json');
-//            Redis::set('data', $data);
-//        }
-//
-//        dd($data);
-        try {
-            $redis = Redis::connection();
-            $redis->ping(); // Test connection
-            echo "Connected to Redis!";
-        } catch (Exception $e) {
-            echo "Connection error: " . $e->getMessage();
+        $data = Cache::get("data");
+        if(!$data){
+            $data = Storage::disk('local')->get('data/data.json');
+            Cache::set('data', $data);
         }
+
+        dd($data);
     }
 }
