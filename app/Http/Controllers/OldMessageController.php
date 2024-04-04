3c3a2b8c1d5fb379cb02b884668794087f7d0b6b
mortezapro <goodarzi.morteza73@gmail.com>
2024-04-04 13:42:25 +0330

    old data

--- app/Http/Controllers/OldMessageController.php
+++ app/Http/Controllers/OldMessageController.php
@@ -14,30 +14,17 @@ class OldMessageController extends Controller
     public function index()
     {
         $filePath = storage_path("app/data/data.json");
-        $handle = fopen($filePath, "r");
-        if ($handle) {
-            while (($line = fgets($handle)) !== false) {
-                $data = json_decode($line, true);
-                dd($data);
-                foreach ($data['messages'] as $message) {
-                    $text = strtolower($message['text']);
-                    $text = preg_replace("/[^a-zA-Z\s]/", "", $text); // Remove punctuation
-                    $words = explode(" ", $text);
-                    foreach ($words as $word) {
-                        if (!empty($word)) {
-                            $wordFrequencies[$word] = isset($wordFrequencies[$word]) ? $wordFrequencies[$word] + 1 : 1;
-                        }
-                    }
-                }
-            }
-            fclose($handle);
-            arsort($wordFrequencies);
-            $topFifty = array_slice($wordFrequencies, 0, 50);
-            foreach ($topFifty as $word => $frequency) {
-                echo "$word: $frequency\n";
-            }
-        } else {
-            echo "Error opening the file.";
+        $jsonString = File::get($filePath);
+        $data = json_decode($jsonString, true); // Set `true` for associative array
+
+        if (json_last_error() !== JSON_ERROR_NONE) {
+            throw new Exception('Error parsing JSON file: ' . json_last_error_msg());
+        }
+        foreach ($data as $message) {
+//            $telegramMessageId = $message['message_id'];
+//            $senderId = $message['from']['id'];
+            $text = $message['text'] ?? null;
+            echo $text."<br>";
         }
     }
 }
