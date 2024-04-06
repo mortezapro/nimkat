dee807484149db09d836b65022c0ddb50a3ce9f5
mortezapro <goodarzi.morteza73@gmail.com>
2024-04-06 10:07:26 +0330

    telegram

--- resources/views/top-words.blade.php
+++ resources/views/top-words.blade.php
@@ -9,9 +9,16 @@
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-
-
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <style>
+        @font-face {
+            src: url("{{ asset("fonts/iranSans.woff2") }}");
+            font-family: "iran-yekan", serif;
+        }
+        body{
+            font-family: "iran-yekan", serif;
+        }
+    </style>
 </head>
 <body class="font-sans antialiased dark:bg-black dark:text-white/50">
 <div class="container">
